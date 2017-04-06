deaa521bb8dda18ccf70848b0a35d499c489ac34
Pierluigi Ferrari <pierluigi.ferrari@gmx.com>
2017-04-06 17:20:28 +0200

    feat: Added anchor box layer

---
+++ keras_layer_AnchorBoxes.py
@@ -0,0 +1,175 @@
+import keras.backend as K
+from keras.engine.topology import InputSpec
+from keras.engine.topology import Layer
+import numpy as np
+
+class AnchorBoxes(Layer):
+    '''
+    Create an output tensor containing anchor boxes based on the input tensor and
+    the passed arguments.
+
+    A set of 2D anchor boxes of different aspect ratios is created for each spatial unit of
+    the input tensor. The number of anchor boxes created per unit depends on the arguments
+    `aspect_ratios` and `two_boxes_for_ar1`, in the default case it is 4. The boxes
+    are parameterized by `(xmin, xmax, ymin, ymax)`.
+
+    The logic implemented by this layer is identical to the logic in the module
+    `ssd_box_encode_decode_utils.py`.
+
+    The purpose of having this layer in the network is to make the model self-sufficient
+    at inference time. Since the model is predicting offsets to the anchor boxes
+    (rather than predicting box coordinates directly), one needs to know the anchor
+    boxes in order to construct the prediction boxes from the offsets. If the model
+    didn't contain this layer, one would always need to be able to generate the appropriate
+    anchor box tensor externally and ad hoc for inference, which would be impossible for
+    someone who only has the model itself. The reason why it is necessary to predict offsets
+    to the anchor boxes rather than to predict box coordinates directly will be explained
+    elsewhere.
+
+    Input shape:
+        4D tensor of shape `(batch, channels, height, width)` if `dim_ordering = 'th'`
+        or `(batch, height, width, channels)` if `dim_ordering = 'tf'`.
+
+    Output shape:
+        5D tensor of shape `(batch, height, width, n_boxes, 4)`.
+    '''
+
+    def __init__(self,
+                 img_height,
+                 img_width,
+                 this_scale,
+                 next_scale,
+                 aspect_ratios=[0.5, 1, 2],
+                 two_boxes_for_ar1=True,
+                 limit_boxes=True,
+                 **kwargs):
+        '''
+        Arguments:
+            img_height (int): The height of the input images.
+            img_width (int): The width of the input images.
+            this_scale (float): A float in [0, 1], the scaling factor for the size of the generate default boxes
+                as a fraction of the shorter side of the input image.
+            next_scale (float): A float in [0, 1], the next larger scaling factor. Only relevant if
+                `self.two_boxes_for_ar1 == True`.
+            aspect_ratios (list, optional): The list of aspect ratios for which default boxes are to be
+                generated. Defaults to [0.5, 1, 2].
+            two_boxes_for_ar1 (bool, optional): Only relevant if `aspect_ratios` contains 1.
+                If `True`, two default boxes will be generated for aspect ratio 1. The first will be generated
+                using the scaling factor for the respective layer, the second one will be generated using
+                geometric mean of said scaling factor and next bigger scaling factor. Defaults to `True`.
+            limit_boxes (bool, optional): If `True`, limits box coordinates to stay within image boundaries.
+                Defaults to `True`.
+        '''
+        if K.backend() != 'tensorflow':
+            raise TypeError("This layer only supports TensorFlow at the moment, but you are using the {} backend.".format(K.backend()))
+        if (this_scale < 0) or (next_scale < 0) or (this_scale > 1) or (next_scale > 1):
+            raise ValueError("`this_scale` and `next_scale` must be in [0, 1], but `this_scale` == {}, `next_scale` == {}".format(this_scale, next_scale))
+        self.img_height = img_height
+        self.img_width = img_width
+        self.this_scale = this_scale
+        self.next_scale = next_scale
+        self.aspect_ratios = aspect_ratios
+        self.two_boxes_for_ar1 = two_boxes_for_ar1
+        self.limit_boxes = limit_boxes
+        # Compute the number of boxes per cell
+        if (1 in aspect_ratios) & two_boxes_for_ar1:
+            self.n_boxes = len(aspect_ratios) + 1
+        else:
+            self.n_boxes = len(aspect_ratios)
+        super(AnchorBoxes, self).__init__(**kwargs)
+
+    def build(self, input_shape):
+        self.input_spec = [InputSpec(shape=input_shape)]
+        super(AnchorBoxes, self).build(input_shape)
+
+    def call(self, x, mask=None):
+        '''
+        Return an anchor box tensor based on the input tensor.
+
+        The logic implemented here is identical to the logic in the module `ssd_box_encode_decode_utils.py`.
+
+        Note that this tensor does not participate in any graph computations at runtime. It is being created
+        as a constant once for each classification conv layer during graph creation and is just being output
+        along with the rest of the model output during runtime. Because of this, all logic is implemented
+        as Numpy array operations and it sufficient to convert the resulting Numpy array into a Keras tensor
+        at the very end before outputting it.
+        '''
+
+        # Compute box width and height for each aspect ratio
+        # The shorter side of the image will be used to compute `w` and `h` using `scale` and `aspect_ratios`.
+        self.aspect_ratios = np.sort(self.aspect_ratios)
+        size = min(self.img_height, self.img_width)
+        wh_list = []
+        for ar in self.aspect_ratios:
+            if (ar == 1) & self.two_boxes_for_ar1:
+                # Compute the regular default box for aspect ratio 1 and...
+                w = self.this_scale * size * np.sqrt(ar)
+                h = self.this_scale * size / np.sqrt(ar)
+                wh_list.append((w,h))
+                # ...also compute one slightly larger version using the geometric mean of this scale value and the next
+                w = np.sqrt(self.this_scale * self.next_scale) * size * np.sqrt(ar)
+                h = np.sqrt(self.this_scale * self.next_scale) * size / np.sqrt(ar)
+                wh_list.append((w,h))
+            else:
+                w = self.this_scale * size * np.sqrt(ar)
+                h = self.this_scale * size / np.sqrt(ar)
+                wh_list.append((w,h))
+        wh_list = np.array(wh_list, dtype=np.int32)
+
+        # We need the shape of the input tensor
+        if K.image_dim_ordering() == 'tf':
+            batch_size, feature_map_height, feature_map_width, feature_map_channels = x._keras_shape
+        else: # Not yet relevant since TensorFlow is the only supported backend right now, but it can't harm to have this in here for the future
+            batch_size, feature_map_channels, feature_map_height, feature_map_width = x._keras_shape
+
+        # Compute the grid of box center points. They are identical for all aspect ratios
+        cell_height = self.img_height / feature_map_height
+        cell_width = self.img_width / feature_map_width
+        cx = (np.linspace(cell_width/2, self.img_width-cell_width/2, feature_map_width)).astype(np.int32)
+        cy = (np.linspace(cell_height/2, self.img_height-cell_height/2, feature_map_height)).astype(np.int32)
+        cx_grid, cy_grid = np.meshgrid(cx, cy)
+        cx_grid = np.expand_dims(cx_grid, -1).astype(np.int32) # This is necessary for np.tile() to do what we want further down
+        cy_grid = np.expand_dims(cy_grid, -1).astype(np.int32) # This is necessary for np.tile() to do what we want further down
+
+        # Create a 4D tensor template of shape `(feature_map_height, feature_map_width, n_boxes, 4)`
+        # where the last dimension will contain `(cx, cy, w, h)`
+        boxes_tensor = np.zeros((feature_map_height, feature_map_width, self.n_boxes, 4), dtype=np.int32)
+
+        boxes_tensor[:, :, :, 0] = np.tile(cx_grid, (1, 1, self.n_boxes)) # Set cx
+        boxes_tensor[:, :, :, 1] = np.tile(cy_grid, (1, 1, self.n_boxes)) # Set cy
+        boxes_tensor[:, :, :, 2] = wh_list[:, 0] # Set w
+        boxes_tensor[:, :, :, 3] = wh_list[:, 1] # Set h
+
+        # Now we convert `(cx, cy, w, h)` into `(xmin, xmax, ymin, ymax)` in order to
+        # to limit the boxes to lie entirely within the image boundaries
+        temp = np.copy(boxes_tensor)
+        temp[:, :, :, 0] = boxes_tensor[:, :, :, 0] - (boxes_tensor[:, :, :, 2] / 2).astype(np.int32) # Set xmin
+        temp[:, :, :, 1] = boxes_tensor[:, :, :, 0] + (boxes_tensor[:, :, :, 2] / 2).astype(np.int32) # Set xmax
+        temp[:, :, :, 2] = boxes_tensor[:, :, :, 1] - (boxes_tensor[:, :, :, 3] / 2).astype(np.int32) # Set ymin
+        temp[:, :, :, 3] = boxes_tensor[:, :, :, 1] + (boxes_tensor[:, :, :, 3] / 2).astype(np.int32) # Set ymax
+        boxes_tensor = temp
+
+        # If `limit_boxes` is enabled, clip the coordinates to lie within the image boundaries
+        if self.limit_boxes:
+            x_coords = boxes_tensor[:,:,:,[0, 1]]
+            x_coords[x_coords >= self.img_width] = self.img_width - 1
+            x_coords[x_coords < 0] = 0
+            boxes_tensor[:,:,:,[0, 1]] = x_coords
+            y_coords = boxes_tensor[:,:,:,[2, 3]]
+            y_coords[y_coords >= self.img_height] = self.img_height - 1
+            y_coords[y_coords < 0] = 0
+            boxes_tensor[:,:,:,[2, 3]] = y_coords
+
+        # Now prepend one dimension to `boxes_tensor` to account for the batch size and tile it along
+        # The result will be a 5D tensor of shape `(batch_size, feature_map_height, feature_map_width, n_boxes, 4)`
+        boxes_tensor = np.expand_dims(boxes_tensor, axis=0)
+        boxes_tensor = K.tile(K.constant(boxes_tensor, dtype='float32'), (K.shape(x)[0], 1, 1, 1, 1))
+
+        return boxes_tensor
+
+    def compute_output_shape(self, input_shape):
+        if K.image_dim_ordering() == 'tf':
+            batch_size, feature_map_height, feature_map_width, feature_map_channels = input_shape
+        else: # Not yet relevant since TensorFlow is the only supported backend right now, but it can't harm to have this in here for the future
+            batch_size, feature_map_channels, feature_map_height, feature_map_width = input_shape
+        return (batch_size, feature_map_height, feature_map_width, self.n_boxes, 4)
