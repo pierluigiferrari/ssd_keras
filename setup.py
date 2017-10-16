4d8a4400b9f6b5079c9df027a87533e3b5785b67
Mahmoud Lababidi <mla@mla.im>
2017-10-16 11:37:31 -0400

    add setup file for installation

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='singleshot',
+    version='2017.10.16.0',
+    description='A simple SingleShotDetector package',
+    url='https://github.com/lababidi/singleshot',
+    author='Mahmoud Lababidi',
+    author_email='mla@mla.im',
+    license='GPL',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    install_requires=['keras','sh', 'regex'],
+    python_requires='>=3',
+    entry_points={
+        'console_scripts': [
+            'trainssd=trainssd:console',
+        ],
+    },
+)
