f25f5fa9e19ed8c87c299e864a64913304be12ba
Pierluigi Ferrari <pierluigi.ferrari@gmx.com>
2018-03-12 03:47:58 +0100

    feat: Add new ops and deterministic versions for all ops
    Added new ops for gamma correction and histogram equalization and added deterministic versions of all ops in addition to the probabilistic versions.

---
+++ data_generator/object_detection_2d_photometric_ops.py
@@ -0,0 +1,376 @@
+'''
+Various photometric image transformations, both deterministic and probabilistic.
+
+Copyright (C) 2018 Pierluigi Ferrari
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+from __future__ import division
+import numpy as np
+import cv2
+
+class ConvertColor:
+    '''
+    Converts an image between RGB, HSV and grayscale color spaces. This is just a wrapper
+    around `cv2.cvtColor()`.
+    '''
+    def __init__(self, current='RGB', to='HSV', keep_3ch=True):
+        '''
+        Arguments:
+            current (str, optional): The current color space of the images. Can be
+                one of 'RGB' and 'HSV'.
+            to (str, optional): The target color space of the images. Can be one of
+                'RGB', 'HSV', and 'GRAY'.
+            keep_3ch (bool, optional): Only relevant if `to == GRAY`.
+                If `True`, the resulting grayscale images will have three channels.
+        '''
+        if not ((current in {'RGB', 'HSV'}) and (to in {'RGB', 'HSV', 'GRAY'})):
+            raise NotImplementedError
+        self.current = current
+        self.to = to
+        self.keep_3ch = keep_3ch
+
+    def __call__(self, image, labels=None):
+        if self.current == 'RGB' and self.to == 'HSV':
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2HSV)
+        elif self.current == 'RGB' and self.to == 'GRAY':
+            image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+            if self.keep_3ch:
+                image = np.stack([image] * 3, axis=-1)
+        elif self.current == 'HSV' and self.to == 'RGB':
+            image = cv2.cvtColor(image, cv2.COLOR_HSV2RGB)
+        elif self.current == 'HSV' and self.to == 'GRAY':
+            image = cv2.cvtColor(image, cv2.COLOR_HSV2GRAY)
+            if self.keep_3ch:
+                image = np.stack([image] * 3, axis=-1)
+        if labels is None:
+            return image
+        else:
+            return image, labels
+
+class ConvertDataType:
+    '''
+    Converts an image represented as a Numpy array between `uint8` and `float`.
+    Serves as a helper for certain photometric distortions. This is just a wrapper
+    around `np.ndarray.astype()`.
+    '''
+    def __init__(self, to='uint8'):
+        if not (to == 'uint8' or to == 'float'):
+            raise ValueError("`to` can be either of 'uint8' or 'float'.")
+        self.to = to
+
+    def __call__(self, image, labels=None):
+        if self.to == 'uint8':
+            image = image.astype(np.uint8)
+        else:
+            image = image.astype(np.float)
+        if labels is None:
+            return image
+        else:
+            return image, labels
+
+class Hue:
+    '''
+    Changes the hue of an image.
+
+    Important:
+        - Expects HSV input.
+        - Expects input array to be of `dtype` `float`.
+    '''
+    def __init__(self, delta):
+        if not (0 <= delta <= 180): raise ValueError("`delta` must be in the closed interval `[0, 180]`.")
+        self.delta = delta
+
+    def __call__(self, image, labels=None):
+        image[:, :, 0] = (image[:, :, 0] + self.delta) % 180.0
+        if labels is None:
+            return image
+        else:
+            return image, labels
+
+class RandomHue:
+    '''
+    Randomly changes the hue of an image.
+
+    Important:
+        - Expects HSV input.
+        - Expects input array to be of `dtype` `float`.
+    '''
+    def __init__(self, max_delta=18, prob=0.5):
+        if not (0 <= max_delta <= 180): raise ValueError("`max_delta` must be in the closed interval `[0, 180]`.")
+        self.max_delta = max_delta
+        self.prob = prob
+
+    def __call__(self, image, labels=None):
+        p = np.random.uniform(0,1)
+        if p >= (1.0-self.prob):
+            delta = np.random.uniform(-self.max_delta, self.max_delta)
+            change_hue = Hue(delta=delta)
+            return change_hue(image, labels)
+        elif labels is None:
+            return image
+        else:
+            return image, labels
+
+class Saturation:
+    '''
+    Changes the saturation of an image.
+
+    Important:
+        - Expects HSV input.
+        - Expects input array to be of `dtype` `float`.
+    '''
+    def __init__(self, factor):
+        if factor <= 0.0: raise ValueError("It must be `factor > 0`.")
+        self.factor = factor
+
+    def __call__(self, image, labels=None):
+        image[:,:,1] = np.clip(image[:,:,1] * self.factor, 0, 255)
+        if labels is None:
+            return image
+        else:
+            return image, labels
+
+class RandomSaturation:
+    '''
+    Randomly changes the saturation of an image.
+
+    Important:
+        - Expects HSV input.
+        - Expects input array to be of `dtype` `float`.
+    '''
+    def __init__(self, lower=0.3, upper=2.0, prob=0.5):
+        if lower >= upper: raise ValueError("`upper` must be greater than `lower`.")
+        self.lower = lower
+        self.upper = upper
+        self.prob = prob
+
+    def __call__(self, image, labels=None):
+        p = np.random.uniform(0,1)
+        if p >= (1.0-self.prob):
+            factor = np.random.uniform(self.lower, self.upper)
+            change_saturation = Saturation(factor=factor)
+            return change_saturation(image, labels)
+        elif labels is None:
+            return image
+        else:
+            return image, labels
+
+class Brightness:
+    '''
+    Changes the brightness of an image.
+
+    Important:
+        - Expects RGB input.
+        - Expects input array to be of `dtype` `float`.
+    '''
+    def __init__(self, delta):
+        self.delta = delta
+
+    def __call__(self, image, labels=None):
+        image = np.clip(image + self.delta, 0, 255)
+        if labels is None:
+            return image
+        else:
+            return image, labels
+
+class RandomBrightness:
+    '''
+    Randomly changes the brightness of an image.
+
+    Important:
+        - Expects RGB input.
+        - Expects input array to be of `dtype` `float`.
+    '''
+    def __init__(self, lower=-84, upper=84, prob=0.5):
+        if lower >= upper: raise ValueError("`upper` must be greater than `lower`.")
+        self.lower = float(lower)
+        self.upper = float(upper)
+        self.prob = prob
+
+    def __call__(self, image, labels=None):
+        p = np.random.uniform(0,1)
+        if p >= (1.0-self.prob):
+            delta = np.random.uniform(self.lower, self.upper)
+            change_brightness = Brightness(delta=delta)
+            return change_brightness(image, labels)
+        elif labels is None:
+            return image
+        else:
+            return image, labels
+
+class Contrast:
+    '''
+    Changes the contrast of an image.
+
+    Important:
+        - Expects RGB input.
+        - Expects input array to be of `dtype` `float`.
+    '''
+    def __init__(self, factor):
+        if factor <= 0.0: raise ValueError("It must be `factor > 0`.")
+        self.factor = factor
+
+    def __call__(self, image, labels=None):
+        image = np.clip(127.5 + self.factor * (image - 127.5), 0, 255)
+        if labels is None:
+            return image
+        else:
+            return image, labels
+
+class RandomContrast:
+    '''
+    Randomly changes the contrast of an image.
+
+    Important:
+        - Expects RGB input.
+        - Expects input array to be of `dtype` `float`.
+    '''
+    def __init__(self, lower=0.5, upper=1.5, prob=0.5):
+        if lower >= upper: raise ValueError("`upper` must be greater than `lower`.")
+        self.lower = lower
+        self.upper = upper
+        self.prob = prob
+
+    def __call__(self, image, labels=None):
+        p = np.random.uniform(0,1)
+        if p >= (1.0-self.prob):
+            factor = np.random.uniform(self.lower, self.upper)
+            change_contrast = Contrast(factor=factor)
+            return change_contrast(image, labels)
+        elif labels is None:
+            return image
+        else:
+            return image, labels
+
+class Gamma:
+    '''
+    Changes the gamma value of an image.
+
+    Important: Expects RGB input.
+    '''
+    def __init__(self, gamma):
+        self.gamma = gamma
+        self.gamma_inv = 1.0 / gamma
+        # Build a lookup table mapping the pixel values [0, 255] to
+    	# their adjusted gamma values.
+    	self.table = np.array([((i / 255.0) ** self.gamma_inv) * 255 for i in np.arange(0, 256)]).astype("uint8")
+
+    def __call__(self, image, labels=None):
+        image = cv2.LUT(image, table)
+    	if labels is None:
+            return image
+        else:
+            return image, labels
+
+class RandomGamma:
+    '''
+    Randomly changes the gamma value of an image.
+
+    Important: Expects RGB input.
+    '''
+    def __init__(self, lower=0.25, upper=2.0, prob=0.5):
+        if lower >= upper: raise ValueError("`upper` must be greater than `lower`.")
+        self.lower = lower
+        self.upper = upper
+        self.prob = prob
+
+    def __call__(self, image, labels=None):
+        p = np.random.uniform(0,1)
+        if p >= (1.0-self.prob):
+            gamma = np.random.uniform(self.lower, self.upper)
+            change_gamma = Gamma(gamma=gamma)
+            return change_gamma(image, labels)
+        elif labels is None:
+            return image
+        else:
+            return image, labels
+
+class HistogramEqualization:
+    '''
+    Performs histogram equalization on an image.
+
+    Importat: Expects HSV input.
+    '''
+    def __init__(self):
+        pass
+
+    def __call__(self, image, labels=None):
+        image[:,:,2] = cv2.equalizeHist(image[:,:,2])
+        if labels is None:
+            return image
+        else:
+            return image, labels
+
+class RandomHistogramEqualization:
+    '''
+    Randomly performs histogram equalization on an image. The randomness only refers
+    to whether or not the equalization is performed.
+
+    Importat: Expects HSV input.
+    '''
+    def __init__(self, prob=0.5):
+        self.prob = prob
+
+    def __call__(self, image, labels=None):
+        p = np.random.uniform(0,1)
+        if p >= (1.0-self.prob):
+            equalize = HistogramEqualization()
+            return equalize(image, labels)
+        elif labels is None:
+            return image
+        else:
+            return image, labels
+
+class ChannelSwap:
+    '''
+    Swaps the channels of an image.
+
+    Important: Expects RGB input.
+    '''
+    def __init__(self, order):
+        self.order = order
+
+    def __call__(self, image, labels=None):
+        image = image[:,:,self.order]
+        if labels is None:
+            return image
+        else:
+            return image, labels
+
+class RandomChannelSwap:
+    '''
+    Randomly swaps the channels of an image.
+
+    Important: Expects RGB input.
+    '''
+    def __init__(self, prob=0.5):
+        self.prob = prob
+        # All possible permutations of the three image channels except the original order.
+        self.permutations = ((0, 2, 1),
+                             (1, 0, 2), (1, 2, 0),
+                             (2, 0, 1), (2, 1, 0))
+
+    def __call__(self, image, labels=None):
+        p = np.random.uniform(0,1)
+        if p >= (1.0-self.prob):
+            i = np.random.randint(5) # There are 6 possible permutations.
+            permutation = self.permutations[i]
+            swap_channels = ChannelSwap(order=permutation)
+            return swap_channels(image, labels)
+        elif labels is None:
+            return image
+        else:
+            return image, labels
