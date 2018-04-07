05bfda707172d3778c1d3ee66dfcb4d39737bcf5
Pierluigi Ferrari <pierluigi.ferrari@gmx.com>
2018-04-07 21:30:47 +0200

    feat: Add multi-matching function

--- ssd_encoder_decoder/matching_utils.py
+++ ssd_encoder_decoder/matching_utils.py
@@ -57,7 +57,7 @@ def match_bipartite_greedy(weight_matrix):
 
     # This 1D array will contain for each ground truth box the index of
     # the matched anchor box.
-    matches = np.zeros(ground_truth_boxes.shape[0])
+    matches = np.zeros(num_ground_truth_boxes, dtype=np.int)
 
     # In each iteration of the loop below, exactly one ground truth box
     # will be matched to one anchor box.
@@ -78,3 +78,40 @@ def match_bipartite_greedy(weight_matrix):
         weight_matrix[:,anchor_index] = 0
 
     return matches
+
+def match_multi(weight_matrix, threshold):
+    '''
+    Matches all elements along the second axis of `weight_matrix` to their best
+    matches along the first axis subject to the constraint that the weight of a match
+    must be greater than or equal to `threshold` in order to produce a match.
+
+    If the weight matrix contains elements that should be ignored, the row or column
+    representing the respective elemet should be set to a value below `threshold`.
+
+    Arguments:
+        weight_matrix (array): A 2D Numpy array that represents the weight matrix
+            for the matching process. If `(m,n)` is the shape of the weight matrix,
+            it must be `m <= n`. The weights can be integers or floating point
+            numbers. The matching process will maximize, i.e. larger weights are
+            preferred over smaller weights.
+        threshold (float): A float that represents the threshold (i.e. lower bound)
+            that must be met by a pair of elements to produce a match.
+
+    Returns:
+        Two 1D Numpy arrays of equal length that represent the matched indices. The first
+        array contains the indices along the first axis of `weight_matrix`, the second array
+        contains the indices along the second axis.
+    '''
+
+    num_anchor_boxes = weight_matrix.shape[1]
+    all_anchor_indices = list(range(num_anchor_boxes)) # Only relevant for fancy-indexing below.
+
+    # Find the best ground truth match for every anchor box.
+    ground_truth_indices = np.argmax(weight_matrix, axis=0) # Array of shape (weight_matrix.shape[1],)
+    overlaps = weight_matrix[ground_truth_indices, all_anchor_indices] # Array of shape (weight_matrix.shape[1],)
+
+    # Filter out the matches with a weight below the threshold.
+    anchor_indices_thresh_met = np.nonzero(overlaps >= threshold)[0]
+    gt_indices_thresh_met = ground_truth_indices[anchor_indices_thresh_met]
+
+    return gt_indices_thresh_met, anchor_indices_thresh_met
