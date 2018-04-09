ec6e6dd22b09f2f95713c3e60ca2a7300e5ccbbb
Pierluigi Ferrari <pierluigi.ferrari@gmx.com>
2018-04-09 03:17:18 +0200

    feat: Add checks for area and degenerate boxes to `BoxFilter`
    `BoxFilter` is no longer limited to checking the overlap between boxes and an image. It can now also check and filter out degenerate boxes, and it can filter out boxes that have an area below a given threshold.

--- data_generator/object_detection_2d_image_boxes_validation_utils.py
+++ data_generator/object_detection_2d_image_boxes_validation_utils.py
@@ -79,50 +79,79 @@ class BoundGenerator:
 
 class BoxFilter:
     '''
-    Returns all bounding boxes that are valid with respect to a given image height
-    and width according to the given criteria.
+    Returns all bounding boxes that are valid with respect to a the defined criteria.
     '''
 
     def __init__(self,
+                 check_overlap=True,
+                 check_min_area=True,
+                 check_degenerate=True,
                  overlap_criterion='center_point',
-                 bounds=(0.3, 1.0),
+                 overlap_bounds=(0.3, 1.0),
+                 min_area=16,
                  labels_format={'class_id': 0, 'xmin': 1, 'ymin': 2, 'xmax': 3, 'ymax': 4}):
         '''
         Arguments:
+            check_overlap (bool, optional): Whether or not to enforce the overlap requirements defined by
+                `overlap_criterion` and `overlap_bounds`. Sometimes you might want to use the box filter only
+                to enforce a certain minimum area for all boxes (see next argument), in such cases you can
+                turn the overlap requirements off.
+            check_min_area (bool, optional): Whether or not to enforce the minimum area requirement defined
+                by `min_area`. If `True`, any boxes that have an area (in pixels) that is smaller than `min_area`
+                will be removed from the labels of an image. Bounding boxes below a certain area aren't useful
+                training examples. An object that takes up only, say, 5 pixels in an image is probably not
+                recognizable anymore, neither for a human, nor for an object detection model. It makes sense
+                to remove such boxes.
+            check_degenerate (bool, optional): Whether or not to check for and remove degenerate bounding boxes.
+                Degenerate bounding boxes are boxes that have `xmax <= xmin` and/or `ymax <= ymin`. In particular,
+                boxes with a width and/or height of zero are degenerate. It is obviously important to filter out
+                such boxes, so you should only set this option to `False` if you are certain that degenerate
+                boxes are not possible in your data and processing chain.
             overlap_criterion (str, optional): Can be either of 'center_point', 'iou', or 'area'. Determines
                 which boxes are considered valid with respect to a given image. If set to 'center_point',
                 a given bounding box is considered valid if its center point lies within the image.
                 If set to 'area', a given bounding box is considered valid if the quotient of its intersection
-                area with the image and its own area is within `lower` and `upper`. If set to 'iou', a given
-                bounding box is considered valid if its IoU with the image is within `lower` and `upper`.
-            bounds (list or BoundGenerator, optional): Only relevant if `overlap_criterion` is 'area' or 'iou'.
+                area with the image and its own area is within the given `overlap_bounds`. If set to 'iou', a given
+                bounding box is considered valid if its IoU with the image is within the given `overlap_bounds`.
+            overlap_bounds (list or BoundGenerator, optional): Only relevant if `overlap_criterion` is 'area' or 'iou'.
                 Determines the lower and upper bounds for `overlap_criterion`. Can be either a 2-tuple of scalars
                 representing a lower bound and an upper bound, or a `BoundGenerator` object, which provides
                 the possibility to generate bounds randomly.
+            min_area (int, optional): Only relevant if `check_min_area` is `True`. Defines the minimum area in
+                pixels that a bounding box must have in order to be valid. Boxes with an area smaller than this
+                will be removed.
             labels_format (dict, optional): A dictionary that defines which index in the last axis of the labels
                 of an image contains which bounding box coordinate. The dictionary maps at least the keywords
                 'xmin', 'ymin', 'xmax', and 'ymax' to their respective indices within last axis of the labels array.
         '''
-        if not isinstance(bounds, (list, tuple, BoundGenerator)):
-            raise ValueError("`bounds` must be either a 2-tuple of scalars or a `BoundGenerator` object.")
-        if isinstance(bounds, (list, tuple)) and (bounds[0] > bounds[1]):
+        if not isinstance(overlap_bounds, (list, tuple, BoundGenerator)):
+            raise ValueError("`overlap_bounds` must be either a 2-tuple of scalars or a `BoundGenerator` object.")
+        if isinstance(overlap_bounds, (list, tuple)) and (overlap_bounds[0] > overlap_bounds[1]):
             raise ValueError("The lower bound must not be greater than the upper bound.")
         if not (overlap_criterion in {'iou', 'area', 'center_point'}):
             raise ValueError("`overlap_criterion` must be one of 'iou', 'area', or 'center_point'.")
         self.overlap_criterion = overlap_criterion
-        self.bounds = bounds
+        self.overlap_bounds = overlap_bounds
+        self.min_area = min_area
+        self.check_overlap = check_overlap
+        self.check_min_area = check_min_area
+        self.check_degenerate = check_degenerate
         self.labels_format = labels_format
 
     def __call__(self,
-                 image_height,
-                 image_width,
-                 labels):
+                 labels,
+                 image_height=None,
+                 image_width=None):
         '''
         Arguments:
-            image_height (int): The height of the image to compare the box coordinates to.
-            image_width (int): The width of the image to compare the box coordinates to.
-            labels (array): The labels to be tested. The box coordinates are expected
+            labels (array): The labels to be filtered. This is an array with shape `(m,n)`, where
+                `m` is the number of bounding boxes and `n` is the number of elements that defines
+                each bounding box (box coordinates, class ID, etc.). The box coordinates are expected
                 to be in the image's coordinate system.
+            image_height (int): Only relevant if `check_overlap == True`. The height of the image
+                (in pixels) to compare the box coordinates to.
+            image_width (int): `check_overlap == True`. The width of the image (in pixels) to compare
+                the box coordinates to.
 
         Returns:
             An array containing the labels of all boxes that are valid.
@@ -135,43 +164,58 @@ class BoxFilter:
         xmax = self.labels_format['xmax']
         ymax = self.labels_format['ymax']
 
-        # Get the lower and upper bounds.
-        if isinstance(self.bounds, BoundGenerator):
-            lower, upper = self.bounds()
-        else:
-            lower, upper = self.bounds
+        # Record the boxes that pass all checks here.
+        requirements_met = np.ones(shape=labels.shape[0], dtype=np.bool)
 
-        # Compute which boxes are valid.
+        if self.check_degenerate:
 
-        if self.overlap_criterion == 'iou':
-            # Compute the patch coordinates.
-            image_coords = np.array([0, 0, image_width, image_height])
-            # Compute the IoU between the patch and all of the ground truth boxes.
-            image_boxes_iou = iou(image_coords, labels[:, [xmin, ymin, xmax, ymax]], coords='corners', mode='element-wise')
-            requirements_met = (image_boxes_iou > lower) * (image_boxes_iou <= upper)
+            non_degenerate = (labels[:,xmax] > labels[:,xmin]) * (labels[:,ymax] > labels[:,ymin])
+            requirements_met *= non_degenerate
 
-        elif self.overlap_criterion == 'area':
-            # Compute the areas of the boxes.
-            box_areas = (labels[:,xmax] - labels[:,xmin]) * (labels[:,ymax] - labels[:,ymin])
-            # Compute the intersection area between the patch and all of the ground truth boxes.
-            clipped_boxes = np.copy(labels)
-            clipped_boxes[:,[ymin,ymax]] = np.clip(labels[:,[ymin,ymax]], a_min=0, a_max=image_height-1)
-            clipped_boxes[:,[xmin,xmax]] = np.clip(labels[:,[xmin,xmax]], a_min=0, a_max=image_width-1)
-            intersection_areas = (clipped_boxes[:,xmax] - clipped_boxes[:,xmin]) * (clipped_boxes[:,ymax] - clipped_boxes[:,ymin])
-            # Check which boxes meet the overlap requirements.
-            if lower == 0.0:
-                mask_lower = intersection_areas > lower * box_areas # If `self.lower == 0`, we want to make sure that boxes with area 0 don't count, hence the ">" sign instead of the ">=" sign.
+        if self.check_min_area:
+
+            min_area_met = (labels[:,xmax] - labels[:,xmin]) * (labels[:,ymax] - labels[:,ymin]) >= self.min_area
+            requirements_met *= min_area_met
+
+        if self.check_overlap:
+
+            # Get the lower and upper bounds.
+            if isinstance(self.overlap_bounds, BoundGenerator):
+                lower, upper = self.overlap_bounds()
             else:
-                mask_lower = intersection_areas >= lower * box_areas # Especially for the case `self.lower == 1` we want the ">=" sign, otherwise no boxes would count at all.
-            mask_upper = intersection_areas <= upper * box_areas
-            requirements_met = mask_lower * mask_upper
+                lower, upper = self.overlap_bounds
 
-        elif self.overlap_criterion == 'center_point':
-            # Compute the center points of the boxes.
-            cy = (labels[:,ymin] + labels[:,ymax]) / 2
-            cx = (labels[:,xmin] + labels[:,xmax]) / 2
-            # Check which of the boxes have center points within the cropped patch remove those that don't.
-            requirements_met = (cy >= 0.0) * (cy <= image_height-1) * (cx >= 0.0) * (cx <= image_width-1)
+            # Compute which boxes are valid.
+
+            if self.overlap_criterion == 'iou':
+                # Compute the patch coordinates.
+                image_coords = np.array([0, 0, image_width, image_height])
+                # Compute the IoU between the patch and all of the ground truth boxes.
+                image_boxes_iou = iou(image_coords, labels[:, [xmin, ymin, xmax, ymax]], coords='corners', mode='element-wise')
+                requirements_met *= (image_boxes_iou > lower) * (image_boxes_iou <= upper)
+
+            elif self.overlap_criterion == 'area':
+                # Compute the areas of the boxes.
+                box_areas = (labels[:,xmax] - labels[:,xmin]) * (labels[:,ymax] - labels[:,ymin])
+                # Compute the intersection area between the patch and all of the ground truth boxes.
+                clipped_boxes = np.copy(labels)
+                clipped_boxes[:,[ymin,ymax]] = np.clip(labels[:,[ymin,ymax]], a_min=0, a_max=image_height-1)
+                clipped_boxes[:,[xmin,xmax]] = np.clip(labels[:,[xmin,xmax]], a_min=0, a_max=image_width-1)
+                intersection_areas = (clipped_boxes[:,xmax] - clipped_boxes[:,xmin]) * (clipped_boxes[:,ymax] - clipped_boxes[:,ymin])
+                # Check which boxes meet the overlap requirements.
+                if lower == 0.0:
+                    mask_lower = intersection_areas > lower * box_areas # If `self.lower == 0`, we want to make sure that boxes with area 0 don't count, hence the ">" sign instead of the ">=" sign.
+                else:
+                    mask_lower = intersection_areas >= lower * box_areas # Especially for the case `self.lower == 1` we want the ">=" sign, otherwise no boxes would count at all.
+                mask_upper = intersection_areas <= upper * box_areas
+                requirements_met *= mask_lower * mask_upper
+
+            elif self.overlap_criterion == 'center_point':
+                # Compute the center points of the boxes.
+                cy = (labels[:,ymin] + labels[:,ymax]) / 2
+                cx = (labels[:,xmin] + labels[:,xmax]) / 2
+                # Check which of the boxes have center points within the cropped patch remove those that don't.
+                requirements_met *= (cy >= 0.0) * (cy <= image_height-1) * (cx >= 0.0) * (cx <= image_width-1)
 
         return labels[requirements_met]
 
@@ -212,31 +256,36 @@ class ImageValidator:
         self.bounds = bounds
         self.n_boxes_min = n_boxes_min
         self.labels_format = labels_format
+        self.box_filter = BoxFilter(check_overlap=True,
+                                    check_min_area=False,
+                                    check_degenerate=False,
+                                    overlap_criterion=self.overlap_criterion,
+                                    overlap_bounds=self.bounds,
+                                    labels_format=self.labels_format)
 
     def __call__(self,
+                 labels,
                  image_height,
-                 image_width,
-                 labels):
+                 image_width):
         '''
         Arguments:
-            image_height (int): The height of the image to compare the box coordinates to.
-            image_width (int): The width of the image to compare the box coordinates to.
             labels (array): The labels to be tested. The box coordinates are expected
                 to be in the image's coordinate system.
+            image_height (int): The height of the image to compare the box coordinates to.
+            image_width (int): The width of the image to compare the box coordinates to.
 
         Returns:
             A boolean indicating whether an imgae of the given height and width is
             valid with respect to the given bounding boxes.
         '''
 
-        box_filter = BoxFilter(overlap_criterion=self.overlap_criterion,
-                               bounds=self.bounds,
-                               labels_format=self.labels_format)
+        self.box_filter.overlap_bounds = self.bounds
+        self.box_filter.labels_format = self.labels_format
 
         # Get all boxes that meet the overlap requirements.
-        valid_labels = box_filter(image_height=image_height,
-                                  image_width=image_width,
-                                  labels=labels)
+        valid_labels = self.box_filter(labels=labels,
+                                       image_height=image_height,
+                                       image_width=image_width)
 
         # Check whether enough boxes meet the requirements.
         if isinstance(self.n_boxes_min, int):
